4bdb01fac9b15142f1f5a54c69dcbc1a5d825109
Ricardoalso <ricardo.almeidasoares@camptocamp.com>
2025-01-16 13:23:13 +0100

    [MIG] edi_partner_oca: Migration to 18.0

--- edi_partner_oca/__manifest__.py
+++ edi_partner_oca/__manifest__.py
@@ -6,13 +6,13 @@
     "name": "EDI Partners",
     "summary": """
        EDI framework configuration and base logic for partners""",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi-framework",
     "depends": [
         "contacts",
-        "edi_oca",
+        "edi_core_oca",
     ],
     "data": ["views/partner_views.xml"],
 }
